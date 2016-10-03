004c9f9be9eabdb24bd9da4053fe33ef72bd36de
Andrew Hawker <andrew.r.hawker@gmail.com>
2016-10-02 19:27:52 -0700

    Add test modules for async and sync TCP transports.

---
+++ tests/transport/async/test_tcp.py
@@ -0,0 +1,8 @@
+"""
+    tests/transport/async/test_tcp
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Contains tests for the :mod:`~adbpy.transport.async.tcp` module.
+"""
+
+from adbpy.transport.async import tcp
---
+++ tests/transport/sync/test_tcp.py
@@ -0,0 +1,8 @@
+"""
+    tests/transport/sync/test_tcp
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Contains tests for the :mod:`~adbpy.transport.sync.tcp` module.
+"""
+
+from adbpy.transport.sync import tcp
